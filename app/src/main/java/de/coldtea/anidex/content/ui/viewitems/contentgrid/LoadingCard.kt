941eb6b4dcfc04dad3f46c7321527bb58353c6a1
Yasar Naci Gündüz <yasarnacigunduz@gmail.com>
2021-12-17 16:40:31 +0100

    improved previews

--- app/src/main/java/de/coldtea/anidex/content/ui/viewitems/contentgrid/LoadingCard.kt
+++ app/src/main/java/de/coldtea/anidex/content/ui/viewitems/contentgrid/LoadingCard.kt
@@ -1,12 +1,13 @@
 package de.coldtea.anidex.content.ui.viewitems.contentgrid
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.animation.core.FastOutLinearInEasing
+import androidx.compose.animation.animateColor
 import androidx.compose.animation.core.LinearEasing
-import androidx.compose.animation.core.animateFloat
 import androidx.compose.animation.core.infiniteRepeatable
 import androidx.compose.animation.core.keyframes
 import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -16,11 +17,13 @@ import androidx.compose.material.Card
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import de.coldtea.anidex.base.ui.theme.blueGray002
+import de.coldtea.anidex.base.ui.theme.blueGrayDark
+import de.coldtea.anidex.base.ui.theme.materialBlack
 
 //https://cdn.myanimelist.net/images/anime/10/47347.jpg
 @OptIn(ExperimentalAnimationApi::class)
@@ -30,27 +33,28 @@ fun LoadingCard(
     height: Int,
     width: Int
 ) {
+    val initialColor = if(isSystemInDarkTheme()) blueGrayDark else blueGray002
+    val duration = if(isSystemInDarkTheme()) 4_000 else 2_000
+
     val infiniteTransition = rememberInfiniteTransition()
-    val alphaChannel by infiniteTransition.animateFloat(
-        initialValue = 0.3f,
-        targetValue = 0.3f,
+    val color by infiniteTransition.animateColor(
+        initialValue = initialColor,
+        targetValue = materialBlack,
         animationSpec = infiniteRepeatable(
             keyframes {
-                durationMillis = 1500
-                0.3f at 0 with LinearEasing
-                1f at 750 with FastOutLinearInEasing
-                0.3f at 1500
+                durationMillis = duration
+                initialColor at 0 with LinearEasing
+                materialBlack at duration/2 with LinearEasing
+                initialColor at duration
             }
         )
     )
 
-    Row(
-        Modifier.alpha(alphaChannel)
-    ) {
-        for (i in 0..3) {
+    Row {
+        for (i in 0..2) {
             Card(
                 elevation = 20.dp,
-                backgroundColor = Color.Black,
+                backgroundColor = color,
                 modifier =
                 modifier
                     .padding(start = 6.dp, end = 6.dp, top = 12.dp)
@@ -63,8 +67,21 @@ fun LoadingCard(
 
 }
 
-@Preview
+@Preview(
+    showSystemUi = true,
+    device = Devices.PIXEL_3
+)
 @Composable
-fun PreviewLoadingCard(){
+fun PreviewLoadingCardLight(){
+    LoadingCard(height = 180, width = 115)
+}
+
+@Preview(
+    showSystemUi = true,
+    device = Devices.PIXEL_3,
+    uiMode = UI_MODE_NIGHT_YES
+)
+@Composable
+fun PreviewLoadingCardDark(){
     LoadingCard(height = 180, width = 115)
 }
